600799ede04716cdf860b0150500cfb7d0468f06
arifhussain1987 <mail.arifh@yahoo.com>
2024-11-05 13:10:22 +0300

    Basic React component

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Arif First Mobile Application</Text>
+      <Text style = {{fontsize:90}}>Hello New line</Text>
+      <Button title='Click Me' />
+    </View>
+  )
+}
+
+export default App
